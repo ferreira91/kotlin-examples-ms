578428357ff24e32f7c5887245b832a7d46b69ac
Cainã Ferreira da Silva <cainafs7@gmail.com>
2019-11-13 08:30:11 -0300

    adjust operation

--- order/src/main/kotlin/com/example/order/applicationCore/operations/GetByIdOrderOperation.kt
+++ order/src/main/kotlin/com/example/order/applicationCore/operations/GetByIdOrderOperation.kt
@@ -2,9 +2,11 @@ package com.example.order.applicationCore.operations
 
 import com.example.order.applicationCore.domain.entities.Order
 import com.example.order.applicationCore.persistence.OrderPersistence
+import org.springframework.stereotype.Service
 
+@Service
 class GetByIdOrderOperation(val persistence: OrderPersistence) {
-    fun execute(id: String): Order {
+    fun execute(id: String): Order? {
         return persistence.getById(id)
     }
 }
